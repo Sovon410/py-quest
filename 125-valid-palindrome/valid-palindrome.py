289a0ef4ac2fdea0eeaa98eb02fe295c59308b79
SOVON KAMILA <sovonkamila27@gmail.com>
2026-09-06 00:15:38 +0530

    Time: 13 ms (10.67%) | Memory: 19.6 MB (77.74%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,10 +1,11 @@
 class Solution:
     def isAlphaNum(self, s: str) -> bool:
-        if ('0' <= s <= '9') or ('a' <= s.lower() <= 'z'):
+        if (s >= '0' and s <= '9') or (s >= 'a' and s <= 'z'):
             return True
         return False   
      
     def isPalindrome(self, s: str) -> bool:
+        s = s.lower()
         start, end = 0, len(s) - 1
         while start < end:
             if not self.isAlphaNum(s[start]):
@@ -13,7 +14,7 @@ class Solution:
             if not self.isAlphaNum(s[end]):
                 end -= 1
                 continue
-            if s[start].lower() != s[end].lower():
+            if s[start] != s[end]:
                 return False
             
             start += 1
